bd7431b2e866d220f692d459a53f275b6cdf218c
s18uo <addh3584@gmail.com>
2026-02-01 01:36:30 +0300

    Create generic repository pattern

---
+++ school.sys/Src/Modules/School/Modules.School.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Modules.School.Domain.IRepositories;
+using Modules.School.Infrastructure.Persistent;
+
+namespace Modules.School.Infrastructure.Repositories
+{
+    internal class Repository<T>(SchoolManagementDbContext context) : IRepository<T> where T : class
+    {
+        public async Task AddAsync(T entity)
+        {
+            context.Set<T>().Add(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            return context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(int page = 1, int pageSize = 10)
+        {
+            return await context.Set<T>().Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            context.Set<T>().Update(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
