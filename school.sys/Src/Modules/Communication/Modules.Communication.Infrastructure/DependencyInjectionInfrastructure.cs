a6d6e15c39c7a5ad8eb58d1eb3ebb6a438b1022d
s18uo <addh3584@gmail.com>
2026-02-11 21:20:04 +0300

    Initialize S.W Architecture for communication modules

---
+++ school.sys/Src/Modules/Communication/Modules.Communication.Infrastructure/DependencyInjectionInfrastructure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.Communication.Infrastructure
+{
+    public static class DependencyInjectionInfrastructure
+    {
+    }
+}
