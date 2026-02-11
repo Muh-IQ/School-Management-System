41dcbbacf5daf98c1ff77e8a924ec1670802ffd3
Muh-IQ <mohamedajaj0007@gmail.com>
2026-02-11 21:37:15 +0300

    write_Root_application

---
+++ school.sys/Src/Modules/School/Modules.School.WebAPI/Extensions/SchoolModuleSetup.cs
@@ -0,0 +1,14 @@
+﻿using Modules.School.Application;
+using Modules.School.Infrastructure;
+namespace Modules.School.WebAPI.Extensions
+{
+    public static class SchoolModuleSetup
+    {
+        public static IServiceCollection AddSchoolModule(this IServiceCollection services , IConfiguration configuration)
+        {
+            services.AddInfrastructureServices(configuration);
+            services.AddApplicationServices();
+            return services;
+        }
+    }
+}
--- school.sys/Src/SSM.Host/Program.cs
+++ school.sys/Src/SSM.Host/Program.cs
@@ -1,34 +1,20 @@
-using Modules.School.Application;
-using Modules.School.Infrastructure;
 using Modules.School.WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(Modules.School.WebAPI.Controllers.V1.SchoolController).Assembly);
 
-builder.Services.AddSchoolModule(builder.Configuration);
+
+builder.Services.AddSchoolModule(builder.Configuration); 
 
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyMethod()
-                  .AllowAnyHeader();
-        });
-});
-
 var app = builder.Build();
-app.UseCors("AllowAll");
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -36,7 +22,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.UseHttpsRedirection();
 
