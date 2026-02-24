973c4048cda9446796dd56c0b723478f35348518
Muh-IQ <mohamedajaj0007@gmail.com>
2026-02-24 21:32:22 +0300

    Solving Environment variable in program (SchoolAPI)

---
+++ school.sys/Src/Modules/School/Modules.School.Infrastructure/Persistent/ConnectionProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.School.Infrastructure.Persistent
+{
+    internal class ConnectionProvider
+    {
+        private readonly string _connectionString;
+
+        // Constructor reads the connection string from an environment variable
+        public ConnectionProvider()
+        {
+            _connectionString = Environment.GetEnvironmentVariable("SCHOOL_MODULE____DB_CONNECTION")
+                ?? throw new InvalidOperationException("Database connection string is not set in environment variables.");
+        }
+
+        public string GetConnectionString() => _connectionString;
+    }
+}
