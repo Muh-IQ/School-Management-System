17cde0b147b756643569cb81f3336eec9e6416c2
s18uo <addh3584@gmail.com>
2026-02-27 15:24:51 +0300

    Write Area service and testing

---
+++ school.sys/Src/Modules/School/Modules.School.Application/IServices/IAreaService.cs
@@ -0,0 +1,10 @@
+﻿using Modules.School.Domain.Common.Results;
+using Modules.School.Domain.DTOs;
+
+namespace Modules.School.Application.IServices
+{
+    public interface IAreaService
+    {
+        Task<Result<IEnumerable<LocationDTO>>> GetAllByIdAsync(Guid cityId);
+    }
+}
