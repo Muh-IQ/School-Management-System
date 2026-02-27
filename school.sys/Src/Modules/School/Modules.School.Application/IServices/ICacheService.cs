8d1a76a1573c93c60a8eceb16d4f41f3833e4a13
Muh-IQ <mohamedajaj0007@gmail.com>
2026-02-27 22:34:17 +0300

    merage change

--- school.sys/Src/Modules/School/Modules.School.Application/IServices/ICacheService.cs
+++ school.sys/Src/Modules/School/Modules.School.Application/IServices/ICacheService.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Modules.School.Application.IServices
 {
-    internal class ICacheService
+    internal interface ICacheService
     {
+        Task<T> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, TimeSpan? expiration = null);
+        Task RemoveAsync(string key);
     }
 }
--- school.sys/Src/Modules/School/Modules.School.Application/Services/MemoryCacheService.cs
+++ school.sys/Src/Modules/School/Modules.School.Application/Services/MemoryCacheService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Modules.School.Application.IServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,29 @@ using System.Threading.Tasks;
 
 namespace Modules.School.Application.Services
 {
-    internal class MemoryCacheService
+    internal class MemoryCacheService : ICacheService
     {
+        private readonly IMemoryCache _memoryCache;
+
+        public MemoryCacheService(IMemoryCache memoryCache) => _memoryCache = memoryCache;
+
+        public async Task<T> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, TimeSpan? expiration = null)
+        {
+            if (_memoryCache.TryGetValue(key, out T result)) return result;
+
+            result = await factory();
+
+            var options = new MemoryCacheEntryOptions
+            { AbsoluteExpirationRelativeToNow = expiration ?? TimeSpan.FromMinutes(10) };
+
+            _memoryCache.Set(key, result, options);
+            return result;
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            _memoryCache.Remove(key);
+            return Task.CompletedTask;
+        }
     }
 }
