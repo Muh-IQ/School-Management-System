425c09ad60bd1b9837dfb15cc7d317dfe1701689
s18uo <addh3584@gmail.com>
2026-03-23 11:04:43 +0300

    write schoolUser service

---
+++ school.sys/Src/Modules/School/Modules.School.Application/Services/UserSchoolService.cs
@@ -0,0 +1,40 @@
+﻿using Modules.School.Application.IServices;
+using Modules.School.Domain.Common.Results;
+using Modules.School.Domain.Common.StaticError;
+using Modules.School.Domain.Entities;
+using Modules.School.Domain.IRepositories;
+using Modules.School.Domain.IThirdPartyServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.School.Application.Services
+{
+    public class UserSchoolService(IGenericRepository<UserSchool> genericRepository,ITimeProvider timeProvider) : IUserSchoolService
+    {
+        public async Task<Result> AddAsync(Guid userId, Guid SchoolId)
+        {
+
+            var user = new UserSchool()
+            {
+                UserId = userId,
+                SchoolId = SchoolId,
+                IsActive = true,
+                IsDeleted = false,
+                CreateAt = timeProvider.UtcNow,
+                UpdateAt = null
+            };
+
+            var result = await genericRepository.AddAsync(user);
+
+            if(result==false)
+            {
+                return Result.Failure(ErrorType.InternalServerError,UserErrors.InternalServerErrorMessage() ); 
+            }
+
+            return Result.Success();
+        }
+    }
+}
