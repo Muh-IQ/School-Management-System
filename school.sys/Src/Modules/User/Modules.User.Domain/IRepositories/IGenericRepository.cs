7559cf2c3d59f442880e464991a47cb56c6a0c47
aboody12326 <aboody.mo29@gmail.com>
2026-03-23 03:15:50 -0400

    CreateGenericUserRepo

---
+++ school.sys/Src/Modules/User/Modules.User.Domain/IRepositories/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.User.Domain.IRepositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<bool> AddAsync(T entity);
+        
+
+    }
+}
---
+++ school.sys/Src/Modules/User/Modules.User.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modules.User.Domain.IRepositories;
+using Modules.User.Infrastructure.Presistent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.User.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly UserDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+        public GenericRepository(UserDbContext context)
+        {
+            this._context = context;
+            this._dbSet = context.Set<T>();
+        }
+        public async Task<bool> AddAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            var result = await _context.SaveChangesAsync();
+            return result > 0;
+        }
+    }
+}
