47b6e77707c11652c5387dcaf2a35dc30679f791
s18uo <addh3584@gmail.com>
2026-03-28 21:28:49 +0300

    add DI of Application

--- school.sys/Src/Modules/User/Modules.User.Application/DependencyInjectionApplication.cs
+++ school.sys/Src/Modules/User/Modules.User.Application/DependencyInjectionApplication.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Modules.User.Application.Services;
+using Modules.User.Domain.IDomainServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,12 @@ using System.Threading.Tasks;
 
 namespace Modules.User.Application
 {
-    public class DependencyInjectionApplication
+    public static class DependencyInjectionApplication
     {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddSingleton<ITimeConverter, Services.TimeConverter>();
+            return services;
+        }
     }
 }
