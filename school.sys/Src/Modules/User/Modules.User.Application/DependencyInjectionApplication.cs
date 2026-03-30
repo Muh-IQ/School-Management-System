273ee51883613ff9a5aa5b1c9197e6527d93b7d6
s18uo <addh3584@gmail.com>
2026-03-30 04:01:04 +0300

    Remove time

--- school.sys/Src/Modules/User/Modules.User.Application/DependencyInjectionApplication.cs
+++ school.sys/Src/Modules/User/Modules.User.Application/DependencyInjectionApplication.cs
@@ -1,11 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Modules.User.Application.Services;
-using Modules.User.Domain.IDomainServices;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Modules.User.Application
 {
@@ -13,7 +6,6 @@ namespace Modules.User.Application
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            services.AddSingleton<ITimeConverter, Services.TimeConverter>();
             return services;
         }
     }
