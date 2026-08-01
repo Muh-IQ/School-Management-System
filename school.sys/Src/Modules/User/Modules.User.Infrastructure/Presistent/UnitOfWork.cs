bac48b0e21515a5d42c946db80096b536841ec1a
Muh-IQ <mohamedajaj0007@gmail.com>
2026-08-01 08:30:13 +0300

    Add UoF to User Moduler

---
+++ school.sys/Src/Modules/User/Modules.User.Infrastructure/Presistent/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using Modules.User.Application.Helpers;
+using Modules.User.Domain.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.User.Infrastructure.Presistent
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly UserDbContext _context;
+
+        public IUserRepository Users { get; }
+
+
+        public UnitOfWork(
+            UserDbContext context,
+            IUserRepository users)
+        {
+            _context = context;
+            Users = users;
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return _context.SaveChangesAsync(cancellationToken);
+        }
+
+      
+    }
+}
