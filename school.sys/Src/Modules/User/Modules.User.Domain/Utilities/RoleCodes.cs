fcc7c07b80d412dade4c3c79d5a07419291479bf
Muh-IQ <mohamedajaj0007@gmail.com>
2026-08-01 09:19:13 +0300

    create role repo in User Moduler

---
+++ school.sys/Src/Modules/User/Modules.User.Domain/Utilities/RoleCodes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.User.Domain.Utilities
+{
+    public static class RoleCodes
+    {
+        public const string SuperAdmin = "SUPER_ADMIN";
+        public const string SchoolAdmin = "SCHOOL_ADMIN";
+        public const string Teacher = "TEACHER";
+        public const string Student = "STUDENT";
+        public const string Parent = "PARENT";
+    }
+}
