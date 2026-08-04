e6d381228bf597aa3f990d457f0b64915d3a73c5
Muh-IQ <mohamedajaj0007@gmail.com>
2026-08-04 14:09:54 +0300

    UserRole Repo created

--- school.sys/Src/Modules/User/Modules.User.Infrastructure/DependencyInjectionInfrastructure.cs
+++ school.sys/Src/Modules/User/Modules.User.Infrastructure/DependencyInjectionInfrastructure.cs
@@ -26,6 +26,9 @@ namespace Modules.User.Infrastructure
                 options.UseSqlServer(connectionProvider.GetConnectionString());
             });
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
             return services;
         }
     }
