287aa7d1141504b17841a401cca0b5ecb6bea84a
s18uo <addh3584@gmail.com>
2026-08-25 00:59:50 +0300

    Create Integration test

---
+++ school.sys/Tests/User/User.IntegrationTests/Api/User Module/RoleControllerTests.cs
@@ -0,0 +1,68 @@
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Modules.User.Domain.Entities;
+using Modules.User.Infrastructure.Presistent;
+using System.Net;
+using System.Text.Json;
+using Xunit;
+
+namespace User.IntegrationTests.Api.User_Module;
+
+public class RoleControllerTests : IClassFixture<UserWebApplicationFactory>
+{
+
+    #region GetByCode
+    [Fact]
+    public async Task GetByCode_Should_Return200_With_Data_When_RoleExists()
+    {
+        using var factory = new UserWebApplicationFactory();
+        var client = factory.CreateClient();
+        var roleId = Guid.NewGuid();
+
+        using (var scope = factory.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+            context.Roles.Add(new Role
+            {
+                Id = roleId,
+                Name = "Teacher",
+                Code = "TEACHER",
+                IsActive = true,
+                IsDeleted = false,
+                CreateAt = DateTime.UtcNow
+            });
+            await context.SaveChangesAsync();
+        }
+
+        var response = await client.GetAsync("/api/v1/role/TEACHER");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var body = await ReadJsonAsync(response);
+        body.RootElement.GetProperty("success").GetBoolean().Should().BeTrue();
+        var data = body.RootElement.GetProperty("data");
+        data.GetProperty("id").GetGuid().Should().Be(roleId);
+        data.GetProperty("name").GetString().Should().Be("Teacher");
+    }
+
+    [Fact]
+    public async Task GetByCode_Should_Return404_When_RoleDoesNotExist()
+    {
+        using var factory = new UserWebApplicationFactory();
+        var client = factory.CreateClient();
+
+        var response = await client.GetAsync("/api/v1/role/UNKNOWN");
+
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        var body = await ReadJsonAsync(response);
+        body.RootElement.GetProperty("success").GetBoolean().Should().BeFalse();
+        body.RootElement.GetProperty("message").GetString()
+            .Should().Be("Role with code 'UNKNOWN' not found.");
+    }
+    #endregion
+
+    #region Helper
+
+    private static async Task<JsonDocument> ReadJsonAsync(HttpResponseMessage response) =>
+        JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+    #endregion
+}
---
+++ school.sys/Tests/User/User.IntegrationTests/Api/User Module/UserWebApplicationFactory.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Modules.User.Infrastructure.Presistent;
+using SharedKernel;
+using System;
+
+public class UserWebApplicationFactory
+    : WebApplicationFactory<Program>
+{
+    private readonly string _dbName = Guid.NewGuid().ToString();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<IEventBus>();
+            services.AddSingleton<IEventBus, NoOpEventBus>();
+
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<UserDbContext>));
+
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<UserDbContext>(options =>
+            {
+                options.UseInMemoryDatabase(_dbName);
+            });
+        });
+    }
+
+    private sealed class NoOpEventBus : IEventBus
+    {
+        public Task PublishAsync<T>(T integrationEvent, CancellationToken cancellationToken = default)
+            where T : class, IIntegrationEvent => Task.CompletedTask;
+    }
+}
