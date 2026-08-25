0a230c4d4b7add8fc72a6b5ddb5f9b955f71226e
MOHAMMED <srabal3qad6@gmail.com>
2026-08-25 04:46:02 +0300

    Add update user API request DTO

---
+++ school.sys/Src/Modules/User/Modules.User.WebAPI/Common/DTOs/UpdateUserRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Modules.User.WebAPI.Common.DTOs
+{
+    public class UpdateUserRequestDTO
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Gender { get; set; }
+    }
+}
